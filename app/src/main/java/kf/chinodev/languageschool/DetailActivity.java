957e4172f8ab05cb2a8d12cc0871820e6169548a
4inodev <4ino.dev@gmail.com>
2021-02-17 19:36:55 +0600

    add logic

--- app/src/main/java/kf/chinodev/languageschool/DetailActivity.java
+++ app/src/main/java/kf/chinodev/languageschool/DetailActivity.java
@@ -2,13 +2,48 @@ package kf.chinodev.languageschool;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+    ImageView headerImage;
+    TextView headerTitle;
+    TextView contentTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        headerImage = findViewById(R.id.header_image);
+        headerTitle = findViewById(R.id.header_title);
+        contentTextView = findViewById(R.id.content_text_view);
+
+        int viewId = getIntent().getIntExtra("view_id", -1);
+        if (viewId != -1) {
+            switch (viewId) {
+                case R.id.service_camp:
+                    headerImage.setImageResource(R.drawable.camp);
+                    headerTitle.setText("Летний лагерь");
+                    contentTextView.setText(R.string.service_camp);
+                    break;
+                case R.id.service_courses:
+                    headerImage.setImageResource(R.drawable.flag);
+                    headerTitle.setText("Языковые курсы");
+                    contentTextView.setText(R.string.service_languages);
+                    break;
+                case R.id.service_talking:
+                    headerImage.setImageResource(R.drawable.talking);
+                    headerTitle.setText("Talking club");
+                    contentTextView.setText(R.string.service_talking);
+                    break;
+                case R.id.service_toefl:
+                    headerImage.setImageResource(R.drawable.toefl);
+                    headerTitle.setText("TOEFL");
+                    contentTextView.setText(R.string.service_toefl);
+                    break;
+            }
+        }
     }
 }
--- app/src/main/java/kf/chinodev/languageschool/MainActivity.java
+++ app/src/main/java/kf/chinodev/languageschool/MainActivity.java
@@ -1,14 +1,34 @@
 package kf.chinodev.languageschool;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ConstraintLayout courses = findViewById(R.id.service_courses);
+        ConstraintLayout talking = findViewById(R.id.service_talking);
+        ConstraintLayout camp = findViewById(R.id.service_camp);
+        ConstraintLayout toefl = findViewById(R.id.service_toefl);
+
+        courses.setOnClickListener(this);
+        talking.setOnClickListener(this);
+        camp.setOnClickListener(this);
+        toefl.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra("view_id", view.getId());
+        startActivity(intent);
     }
 }
